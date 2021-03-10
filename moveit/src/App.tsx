9d0058c9d18dbfa297e7e183a57103b5730388ca
vinnidias <vinicius.3991@gmail.com>
2021-03-10 12:37:21 -0300

    add ExperienceBar component on App

--- moveit/src/App.tsx
+++ moveit/src/App.tsx
@@ -1,24 +1,10 @@
-import {Button} from './components/Button'
-
+import { ExperienceBar } from './components/ExperienceBar'
+import './styles/global.css'
 
 function App() {
   return (
-    <div>
-      <h1>
-        Hello World
-      </h1>
-
-      <Button color="red">
-        Botão 1
-      </Button>    
-
-      <Button color="blue">
-        Botão 2
-      </Button>   
-
-      <Button color="green">
-        Botão 3
-      </Button>   
+    <div className="container">
+      <ExperienceBar/>
     </div>
   );
 }
