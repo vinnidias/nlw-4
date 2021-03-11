bdfc68412ed20d3b75e6fe0c3413c235eb601b98
vinnidias <vinicius.3991@gmail.com>
2021-03-11 14:29:45 -0300

    Componente do perfil

---
+++ moveit-next/src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/vinnidias.png" alt="Vinicius Dias" />
+            <div>
+                <strong>Vinicius Dias</strong>
+                <p>
+                    <img src="icons/level.png" alt="Level"/>
+                    Level 1
+                </p>
+            </div>
+        </div>
+    )
+}
